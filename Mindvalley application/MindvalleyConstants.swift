62b08cfac80fda1d5d874bcdbe6ca9aba8408737
woogii <jhsw0201@naver.com>
2017-04-15 12:17:24 +0900

    feat : Create project

---
+++ Mindvalley application/MindvalleyConstants.swift
@@ -0,0 +1,9 @@
+//
+//  MindvalleyConstants.swift
+//  Mindvalley application
+//
+//  Created by siwook on 2017. 4. 14..
+//  Copyright © 2017년 siwook. All rights reserved.
+//
+
+import Foundation
